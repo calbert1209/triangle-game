f72d7bcf4c4576bf59d5b7994b59e9cbdb432906
Christian Albert <calbert1209@gmail.com>
2025-12-31 22:26:27 +0900

    remove old finder fn that used potentials

--- src/models/fillableGroup/index.test.ts
+++ src/models/fillableGroup/index.test.ts
@@ -1,86 +1,8 @@
-import {
-  findEdgeCells,
-  findEnclosedUnownedCells,
-  findFillableGroup,
-  findOutsideCells,
-  mapCellPotentials,
-} from ".";
+import { findEdgeCells, findFillableGroup, findOutsideCells } from ".";
 import { TriangleGameState } from "../Game";
 import { TriangleId } from "../Triangle";
 
 const playerId = 0;
-const capturedCells: TriangleGameState["capturedCells"] = {
-  "t-1-23": playerId,
-  "t-2-17": playerId,
-  "t-2-19": playerId,
-  "t-2-21": playerId,
-  "t-2-24": playerId,
-  "t-3-17": playerId,
-  "t-3-19": playerId,
-  "t-3-21": playerId,
-  "t-3-24": playerId,
-  "t-4-24": playerId,
-  "t-5-22": playerId,
-  "t-5-20": playerId,
-  "t-6-21": playerId,
-};
-
-describe(`${mapCellPotentials.name}`, () => {
-  const potentials = mapCellPotentials(capturedCells, playerId);
-  expect(potentials).toHaveProperty("t-2-18", 2);
-  const potentialCounts = Object.values(potentials).reduce(
-    (acc, potential) => {
-      acc[potential] += 1;
-      return acc;
-    },
-    { "0": 0, 1: 0, 2: 0, 3: 0, 4: 0 } as Record<number, number>
-  );
-
-  it("should have correct counts of potentials", () => {
-    expect(potentialCounts).toEqual({
-      0: 767,
-      1: 24,
-      2: 5,
-      3: 1,
-      4: 13,
-    });
-  });
-
-  describe("should give cells specific potentials", () => {
-    const cases = [
-      { cellId: "t-5-21", expected: 3 },
-      { cellId: "t-2-18", expected: 2 },
-      { cellId: "t-2-20", expected: 2 },
-      { cellId: "t-2-23", expected: 2 },
-      { cellId: "t-3-18", expected: 2 },
-      { cellId: "t-3-20", expected: 2 },
-      { cellId: "t-2-22", expected: 1 },
-      { cellId: "t-3-22", expected: 1 },
-      { cellId: "t-3-23", expected: 1 },
-      { cellId: "t-4-21", expected: 1 },
-      { cellId: "t-4-22", expected: 1 },
-      { cellId: "t-4-23", expected: 1 },
-    ];
-    it.each(cases)("$cellId -> $expected", ({ cellId, expected }) => {
-      expect(potentials).toHaveProperty(cellId, expected);
-    });
-  });
-});
-
-describe(`${findFillableGroup.name}`, () => {
-  const fillableGroup = findFillableGroup(capturedCells, playerId);
-  it("should have correct size", () => {
-    expect(fillableGroup.length).toBe(5);
-  });
-  it("should contain specific cells", () => {
-    const expectedCells = ["t-2-18", "t-2-20", "t-3-18", "t-3-20", "t-5-21"];
-
-    for (const cellId of expectedCells) {
-      expect(fillableGroup).toContain(cellId);
-    }
-  });
-});
-
 const sampleRows = 6;
 const sampleCols = 12;
 
@@ -241,7 +163,7 @@ describe(`${findOutsideCells.name}`, () => {
   });
 });
 
-describe(`${findEnclosedUnownedCells.name}`, () => {
+describe(`${findFillableGroup.name}`, () => {
   const capturedCells: TriangleGameState["capturedCells"] = {
     "t-0-2": playerId,
     "t-0-4": playerId,
@@ -253,7 +175,7 @@ describe(`${findEnclosedUnownedCells.name}`, () => {
     "t-3-4": playerId,
   };
 
-  const enclosed = findEnclosedUnownedCells(capturedCells, playerId, 4, 7);
+  const enclosed = findFillableGroup(capturedCells, playerId, 4, 7);
   const expectedIds: TriangleId[] = [
     "t-1-1",
     "t-1-2",
--- src/models/fillableGroup/index.ts
+++ src/models/fillableGroup/index.ts
@@ -2,29 +2,6 @@ import { BOARD_COLS, BOARD_ROWS } from "../constants";
 import { TriangleGameState } from "../Game";
 import { createTriangleFromId, TriangleId } from "../Triangle";
 
-const getCellPotential = (
-  [c, r]: [number, number],
-  capturedCells: TriangleGameState["capturedCells"],
-  playerId: number
-): number => {
-  const cellId: TriangleId = `t-${c}-${r}`;
-  const owner = capturedCells[cellId];
-  if (owner !== undefined) {
-    return owner === playerId ? 4 : 0;
-  }
-
-  const triangle = createTriangleFromId(cellId);
-  let potential = 0;
-
-  for (const neighborId of triangle.neighborIds) {
-    if (capturedCells[neighborId] === playerId) {
-      potential += 1;
-    }
-  }
-
-  return potential;
-};
-
 const iterateThroughBoardCells = (
   cb: (r: number, c: number) => void,
   rows = BOARD_ROWS,
@@ -35,82 +12,6 @@ const iterateThroughBoardCells = (
       cb(row, col);
     }
   }
-};
-
-export const mapCellPotentials = (
-  capturedCells: TriangleGameState["capturedCells"],
-  playerId: number
-) => {
-  const potentials: Record<TriangleId, number> = {};
-  iterateThroughBoardCells((r, c) => {
-    const cellId: TriangleId = `t-${r}-${c}`;
-    potentials[cellId] = getCellPotential([r, c], capturedCells, playerId);
-  });
-
-  return potentials;
-};
-
-export const checkAllAdjacentCells = (
-  cellId: TriangleId,
-  potentials: Record<TriangleId, number>
-): {
-  neighbors: TriangleId[];
-  canFill: boolean;
-} => {
-  const checkedNeighbors = new Set<TriangleId>();
-  const queue: TriangleId[] = [cellId];
-  let openEdgeFound = false;
-  while (!openEdgeFound && queue.length > 0) {
-    const currentCellId = queue.shift() as TriangleId;
-    const triangle = createTriangleFromId(currentCellId);
-    for (const neighborId of triangle.neighborIds) {
-      if (checkedNeighbors.has(neighborId)) {
-        continue;
-      }
-      if (potentials[neighborId] === 4) {
-        continue;
-      }
-      if (potentials[neighborId] === 0) {
-        openEdgeFound = true;
-        break;
-      }
-      checkedNeighbors.add(neighborId);
-      queue.push(neighborId);
-    }
-  }
-
-  return {
-    neighbors: Array.from(checkedNeighbors),
-    canFill: !openEdgeFound,
-  };
-};
-
-export const findFillableGroup = (
-  capturedCells: TriangleGameState["capturedCells"],
-  playerId: number
-): TriangleId[] => {
-  const potentials = mapCellPotentials(capturedCells, playerId);
-  const output = new Set<TriangleId>();
-  const notFillable = new Set<TriangleId>();
-  for (const cellId of Object.keys(potentials) as TriangleId[]) {
-    if (output.has(cellId) || notFillable.has(cellId)) {
-      continue;
-    }
-    if (potentials[cellId] === 4 || potentials[cellId] === 0) {
-      continue;
-    }
-    if (potentials[cellId] === 3) {
-      output.add(cellId);
-    }
-
-    const { neighbors, canFill } = checkAllAdjacentCells(cellId, potentials);
-    const targetSet = canFill ? output : notFillable;
-    neighbors.forEach((cellId) => {
-      targetSet.add(cellId);
-    });
-  }
-
-  return Array.from(output);
 };
 
 export const findEdgeCells = (
@@ -172,7 +73,7 @@ export const findOutsideCells = (
   return outsideCells;
 };
 
-export const findEnclosedUnownedCells = (
+export const findFillableGroup = (
   capturedCells: TriangleGameState["capturedCells"],
   playerId: number,
   rows = BOARD_ROWS,
